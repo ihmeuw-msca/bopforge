6c1f3155e40ccb0288b93f6d59aca9cd9fdcfc6f
zhengp0 <zhengp@uw.edu>
2023-05-17 11:26:20 -0700

    init commit

---
+++ data/continuous/README.md
@@ -0,0 +1,29 @@
+# Data Format
+
+The data file need to stored in name `{rei}-{acause}.csv`.
+This name will be used through out the process.
+
+**Columns of the data frame**
+* `seq`: unique row id
+* `rei_id`: risk id
+* `cause_id`: cause id
+* `study_id`: usually equivalent with `nid` but with minor exceptions
+* `bundle_id`: dataset identifier
+* `bundle_version_id`: dataset identifier at a given point in time
+* `risk_type`: type of the risk, continuous, dichotomous, etc
+* `risk_unit`: Unit of the risk exposure
+* `ln_rr`: mean of log relative risk
+* `ln_rr_se`: standard error of the log relative risk
+* `ref_risk_lower`: lower bound of the exposure range of the reference group
+* `ref_risk_upper`: upper bound of the exposure range of the reference group
+* `alt_risk_lower`: lower bound of the exposure range of the alternative group
+* `alt_risk_upper`: upper bound of the exposure range of the alternative group
+* `bc_{name}`: bias covariates, usually related to study design
+
+
+**Settings**
+
+`settings.yaml` file contains settings for actions in the process, main actions including
+* `fit_signal_model`: fit signal model
+* `select_bias_covs`: select bias covariates
+* `fit_linear_model`: fit final linear model
---
+++ src/bop_pipeline/utils.py
@@ -0,0 +1,93 @@
+"""
+Ultility functions
+"""
+from typing import Dict, Tuple
+
+import numpy as np
+from mrtool import MRBRT, MRBeRT, MRData
+
+
+def fill_dict(des_dict: Dict, default_dict: Dict) -> Dict:
+    """Fill given dictionary by the default dictionary.
+
+    Parameters
+    ----------
+    des_dict : Dict
+        Given dictionary that needs to be filled.
+    default_dict : Dict
+        Default dictionary
+
+    Returns
+    -------
+    Dict
+        Updated dictionary.
+    """
+    for key, value in default_dict.items():
+        if key not in des_dict:
+            des_dict[key] = value
+        elif isinstance(value, dict):
+            des_dict[key] = fill_dict(des_dict[key], value)
+    return des_dict
+
+
+def get_beta_info(model: MRBRT,
+                  cov_name: str = "signal") -> Tuple[float, float]:
+    """Get the posterior information of beta.
+
+    Parameters
+    ----------
+    model : MRBRT
+        MRBRT model, preferably is a simple linear mixed effects model.
+    cov_name : str, optional
+        Name of the interested covariates, default to be "signal".
+
+    Returns
+    -------
+    Tuple[float, float]
+        Return the mean and standard deviation of the corresponding beta.
+    """
+    lt = model.lt
+    index = model.cov_names.index(cov_name)
+    beta = model.beta_soln[index]
+    beta_hessian = lt.hessian(lt.soln)[:lt.k_beta, :lt.k_beta]
+    beta_sd = 1.0 / np.sqrt(beta_hessian[index, index])
+    return (beta, beta_sd)
+
+
+def get_gamma_info(model: MRBRT) -> Tuple[float, float]:
+    """Get the posterior information of gamma.
+
+    Parameters
+    ----------
+    model : MRBRT
+        MRBRT model, preferably is a simple linear mixed effects model. Requires
+        only have one random effect.
+
+    Returns
+    -------
+    Tuple[float, float]
+        Return the mean and standard deviation of the corresponding gamma.
+    """
+    lt = model.lt
+    gamma = model.gamma_soln[0]
+    gamma_fisher = lt.get_gamma_fisher(lt.gamma)
+    gamma_sd = 1.0/np.sqrt(gamma_fisher[0, 0])
+    return (gamma, gamma_sd)
+
+
+def get_signal(signal_model: MRBeRT, risk: np.ndarray) -> np.ndarray:
+    """Get signal from signal_model
+
+    Parameters
+    ----------
+    signal_model : MRBeRT
+        Signal model object.
+    risk : np.ndarray
+        Risk exposures that we want to create signal on.
+    """
+    return signal_model.predict(MRData(covs={
+        "ref_risk_lower": np.repeat(risk.min(), len(risk)),
+        "ref_risk_upper": np.repeat(risk.min(), len(risk)),
+        "alt_risk_lower": risk,
+        "alt_risk_upper": risk
+    }))
